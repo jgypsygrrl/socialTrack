ebe85fb7b8dfebf00f47e5741b2e8b8ce6fe745a
Jennifer <jennifergapay@gmail.com>
2016-02-02 00:16:11 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialTrack::Application.config.secret_key_base = '3ba07e53b6cc6a645f7d9a1365d0e849ed0c68d8db587cef0ffbdb0c63b633fa7b0934b180b0c28914e68f8358b4440d36a329a9de3c98538544f44781b6a126'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialTrack::Application.config.session_store :cookie_store, key: '_socialTrack_session'
